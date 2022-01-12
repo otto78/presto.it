466924cbd75fe4784acbcabcce9f722cac8db25f
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-01-12 20:31:50 +0100

    visualizzaione articoli per categoria fatto

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,9 +10,10 @@ class PublicController extends Controller
     public function home(){
         return view ('home');
     }
-    public function articleByCategory($category, $category_id){
+    public function articlesByCategory($category, $category_id){
         $category=Category::find($category_id);
         $articles=$category->articles()->paginate(5);
-        return view('articles', compact('category', 'articles'));
+        
+        return view('article.articlesByCategory', compact('category', 'articles'));
         }
 }
--- resources/views/article/articlesByCategory.blade.php
+++ resources/views/article/articlesByCategory.blade.php
@@ -19,32 +19,33 @@
                     
                         <div class="col-12 mx-auto d-flex flex-wrap justify-content-evenly">
                             @foreach ($articles as $article)
+                            
                                     <div class="card" style="width: 18rem;">
                                         <img src="/img/segnaposto.png" class="card-img-top" alt="Foto segnaposto">
                                         <div class="card-body">
                                           <h5 class="card-title">{{$article->title}}</h5>
                                           
-                                          @foreach ($article->categories as $category)
+                                          {{-- @foreach ($article->categories as $category)
                                           
                                           <p class="card-subtitle">{{$category->category}}</p>
                                           <br>
                                         
-                                          @endforeach
+                                          @endforeach --}}
                                           <p class="card-text">{{$article->description}}</p>
                                           <p class="card-text">{{$article->price}} €</p>
                                           <p class="card-text">Inserito da: {{$article->user->name}}</p>
 
                                           <hr>
-                                          <a href="{{route('article.articlesByCategory',[
+                                          {{-- <a href="{{route('article.articlesByCategory',[
                                               $articles->category->category,
                                               $articles->category->id,
-                                          ])}}" class="btn btn-primary">{{$articles->category->category}}</a>
+                                          ])}}" class="btn btn-primary">{{$articles->category->category}}</a> --}}
                                         </div>
                                       </div>
                             @endforeach
                         </div>
-                        {{$articles->links()}}
-                </div>
+                    </div>
+                    {{$articles->links()}}
             </div>
         </div>
         
