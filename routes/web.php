ba8fe7eac3d2ffa30a93f8dcab8cf0e27dd84a28
Antonio Servedio <mcorneo93@gmail.com>
2022-01-12 12:03:12 +0100

    errore modifica

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,3 @@ Route::get('/', [PublicController::class, 'home'])->name('home');
 Route::get('/article/index', [ArticleController::class, 'index'])->name('article.index');
 Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
 Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
-Route::get('/article/show/{article}', [ArticleController::class, 'show'])->name('article.show');
-Route::get('/article/edit/{article}', [ArticleController::class, 'edit'])->name('article.edit');
-Route::put('/article/update/{article}', [ArticleController::class, 'update'])->name('article.update');
-Route::delete('/article/destroy/{article}', [ArticleController::class, 'destroy'])->name('article.destroy');
-
-
