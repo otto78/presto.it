3ac694b5d72e14d3959335dab87953386bb78c97
Andrea Garziano <andreagarziano8@gmail.com>
2022-01-13 16:36:47 +0100

    comando makeuserrevisor fatto

---
+++ app/Console/Commands/MakeUserRevisor.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeUserRevisor extends Command
+{
+    
+    protected $signature = 'presto:makeUserRevisor';
+
+    protected $description = 'Rendi un utente revisore';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->ask("Inserisci l'email dell'utente che vuoi rendere revisore");
+
+        $user = User::where('email', $email)->first();
+
+        if(!$user){
+            $this->error("Utente non trovato");
+            return;
+        }
+
+        $user->is_revisor = true;
+        $user->save();
+        $this->info("L'utente {$user->name} è ora un revisore.");
+        
+    }
+}
