f59a16344bf9d570971fed48b4b2a4801326dd3c
Andrea Garziano <andreagarziano8@gmail.com>
2022-01-14 16:47:04 +0100

    tabella User_details con colonna user_id

---
+++ app/Models/UserDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'surname',
+        'gender',
+        'image',
+        'description',
+    ];
+}
