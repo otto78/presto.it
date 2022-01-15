3038922efed6a79f66030638f6fe307363993e47
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-01-15 11:23:51 +0100

    impostazione base per mail

--- resources/views/revisor/workWithUs.blade.php
+++ resources/views/revisor/workWithUs.blade.php
@@ -1,10 +1,22 @@
 <x-layout>
     <div class="container margin-top">
-        <div class="row justify-content-center">
+        <div class="row mb-5 justify-content-center">
             <div class="col-12">
                 <h1 class="text-center">Lavora con noi</h1>
             </div>
-            
         </div>
+        <div class="row mt-5 justify-content-center">
+            <div class="col-12">
+                <p>Hai avuto un'ottima idea a pensare di lavorare con noi. Ci farebbe piacere sepere il perchè!</p>
+            </div>
+            <textarea name="workWithUsMessage" id="" class="form-control" rows="10"></textarea>
+        <div class="col-4 mt-5">
+            <form action="" method="post">
+                <button class="btn btn-presto" type="submit">Invia la tua candidatura</button>
+            </form>
+        </div>
+        </div>
+        
     </div>
+
 </x-layout>
