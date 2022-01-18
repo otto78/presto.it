6b93dfeba179d4f5736aeab4e015a9a9c1e73397
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2022-01-18 17:47:41 +0100

    sistemazione home revisor in corso

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
 
-    <div class="container margin-top">
-        <div class="row justify-content">
+    <div class="container-fluid margin-top bg-login">
+        <div class="row justify-content-center">
             @if ($article)
-            <div class="col-9">
+            <div class="col-8">
                 <div class="card">
                     <div class="card-header">
                         <h2>Annuncio # {{$article->id}}</h2>
+                        <hr>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-3"><h3>Utente</h3></div>
@@ -40,12 +41,12 @@
                             <div class="row">
                                 <div class="col-md-3"><h3>Immagini</h3></div>
                                 <div class="col-md-9">
-
-                                    @foreach($article->images as $image)
-                                        <div class="row md-2 my-4">
-                                            <div class="col-md-4">
+                                    
+                                    <div class="row md-2 my-4">
+                                        @foreach($article->images as $image)
+                                            <div class="col-md-4 ">
                                                 <img 
-                                                    src="{{$image->getUrl(600, 600)}}" class="rounded shadow img-fluid m-2" alt="">
+                                                    src="{{$image->getUrl(300, 300)}}" class="rounded shadow img-fluid m-2" alt="">
                                             </div>
                                             {{-- <div class="col-md-8">
                                                 <p>Id Immagine: {{$image->id}}</p>
@@ -53,8 +54,11 @@
                                                 <p>Percorso Server: {{Storage::url($image->file)}}</p>                                                               
                                             </div> --}}
                                             
-                                        </div>
-                                    @endforeach                                    
+                                        @endforeach 
+                                    </div>
+                                    
+
+                                                                       
                                     
                                 </div>
                             </div>
@@ -62,12 +66,16 @@
                     </div>
                 </div>
             </div>
-            <div class="col-3">
+        </div>        
+        <div class="row justify-content-center">
+            <div class="col-8">
+
                 <form class="my-2" action="{{route('revisor.reject', ['id'=>$article->id])}}" method="post">
                     @csrf
                         @method('put')
                         <button type="submit" class="btn-presto">Rifiuta</button>
                 </form>
+    
                 <form class="my-2" action="{{route('revisor.accept', ['id'=>$article->id])}}" method="post">
                     @csrf
                         @method('put')
@@ -78,9 +86,11 @@
                     @csrf        
                         <button type="submit" class="btn-presto">Rivaluta</button>
                 </form>
+
             </div>
+
+            
         </div>
-        
     </div>
 
     @else 
