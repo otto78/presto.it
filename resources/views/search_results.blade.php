df53ce478d19c485abb37748fee626b07f8d32b9
Andrea Garziano <andreagarziano8@gmail.com>
2022-01-20 19:37:28 +0100

    search finita

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -3,8 +3,8 @@
 {{-- vista risultati ricerca --}}
 
 <div class="container-fluid margin-top bg-login">
-    <h2 class="text-center pt-5 my-5">Risultati ricerca per</h2>
-    <h2 class="text-center my-5">{{$q}}</h2>
+    <h2 class="text-center pt-5 my-5">Risultati ricerca per:</h2>
+    <h2 class="text-center my-5">"{{$q}}"</h2>
     <div class="row justify-content-center">
         
         @if(count($articles)==0)
@@ -14,7 +14,7 @@
                     </div>
         @endif
 
-        <div class="col-12 mx-auto d-flex flex-wrap justify-content-evenly">        
+        {{-- <div class="col-12 mx-auto d-flex flex-wrap justify-content-evenly">        
                 @foreach ($articles as $article)
                         <div class="card my-3" style="width: 18rem;">
                             <img src="/img/segnaposto.png" class="card-img-top" alt="Foto segnaposto">
@@ -36,9 +36,46 @@
                             </div>
                           </div>
                 @endforeach
-            </div>
+        </div> --}}
             
-        
+        <div class="col-12 article">
+            @foreach ($articles as $article)                        
+            <div class="card-article">
+                <div class="circle">
+                    
+                    @foreach ($article->images as $image)
+                        @if($article->images->first()==$image) 
+                            <img src="{{$image->getUrl(300, 300)}}" class="img-presto-card" alt="Foto segnaposto">    
+                        @endif                                                                             
+                    @endforeach
+
+                </div>
+                <div class="dettagli">
+                    <div class="mb-4">
+                        <h4>{{$article->title}}</h4>
+                    </div>
+                    <div class="mb-4">
+                    <h5>{{$article->price}} €</h5>
+                    </div>
+
+                    {{-- <p class="mt-3">{{\Str::limit($article->description, 80)}}</p>                            --}}
+                    <p> Categoria: 
+                        <span>
+                            @foreach ($article->categories as $category)           
+                            <a class="clicca-qui" href="{{route('article.articlesByCategory',[$category->category, $category->id])}}">{{$category->category}}</a>                               
+                            @endforeach                                 
+                        </span>
+                    </p>
+                    <p> Inserito da: 
+                        <span>
+                            <a class="clicca-qui" href="{{route('article.articlesByUser', $article->user->id)}}">{{$article->user->name}}</a>
+                        </span> 
+                    </p>
+                    <a href="{{route('article.show', compact('article'))}}" class="btn-presto shadow">{{__('ui.Dettagli')}}</a>
+                </div>
+            </div>                                      
+            @endforeach
+        </div>
 
     </div>
 </div>
